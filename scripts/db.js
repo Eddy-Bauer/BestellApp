17b6f31e9af62df5418d1dd668f69b278e09682a
Eduard Bauer <eddibauer13@gmail.com>
2026-08-03 15:42:58 +0200

    added js data

---
+++ scripts/db.js
@@ -0,0 +1,24 @@
+let menu = {
+  burgers: [
+    {
+      name: "Cheeseburger",
+      price: 9.90,
+      description: "Saftiges Rinderpatty mit Cheddar Cheese und hausgemachter Sauce"
+    },
+    {
+      name: "Hamburger",
+      price: 9.00,
+      description: "Saftiges Rinderpatty mit Tomaten und Salat auf classic Burgersauce"
+    },
+    {
+      name: "Crispy Chicken",
+      price: 9.50,
+      description: "Knusprig paniertes Hähnchenbrustfilet mit Mayo und Eisbergsalat"
+    },
+    {
+      name: "Veggie Burger",
+      price: 8.50,
+      description: "Hausgemachtes Gemüsepatty mit Avocado-Creme und Rucola"
+    }
+  ]
+}
