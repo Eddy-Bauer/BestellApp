ee6378d07d98442c3a1fa5da6f52c36d8b800ee5
Eduard Bauer <eddibauer13@gmail.com>
2026-08-04 17:34:25 +0200

    feat: add getCalculationTemplate

--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,8 @@
-// 1. Template für die linke Seite 
+// 1. Template für die linke Seite
 function getMenuItemTemplate(burger, index) {
-    let formattedPrice = burger.price.toFixed(2);
+  let formattedPrice = burger.price.toFixed(2);
 
-    return `
+  return `
         <div class="menu-item-card">
             <div class="menu-info">
                 <h3>${burger.name}</h3>
@@ -16,23 +16,43 @@ function getMenuItemTemplate(burger, index) {
 
 // 2. Template für die rechte Seite (Warenkorb)
 function getBasketItemTemplate(basketItem, index) {
-    let totalPrice = (basketItem.price * basketItem.amount).toFixed(2);
+  let totalPrice = (basketItem.price * basketItem.amount).toFixed(2);
 
-    return `
+  return `
         <div class="basket-item">
-            <div class="item-name">
-                <strong>${basketItem.amount} x ${basketItem.name}</strong>
-            </div>
+            <div class="item-name">${basketItem.amount} x ${basketItem.name}</div>
             <div class="item-details">
                 <div class="item-controls">
                     <button class="btn-icon" onclick="decreaseAmount(${index})">-</button>
                     <span>${basketItem.amount}</span>
                     <button class="btn-icon" onclick="increaseAmount(${index})">+</button>
                 </div>
-                <div class="item-price">
-                    ${totalPrice}€
-                </div>
+                <div class="item-price">${totalPrice}€</div>
             </div>
         </div>
+    `;
+}
+
+// Template für den Amount
+function getCalculationTemplate(basket) {
+  let subtotal = basket.reduce((sum, item) => sum + item.price * item.amount,0,);
+  let deliveryFee = 4.99;
+  let total = subtotal + deliveryFee;
+
+  return `
+        <div class="calc-row">
+            <span>Subtotal</span>
+            <span>${subtotal.toFixed(2)}</span>
+        </div>
+        <div class="calc-row">
+            <span>Delivery fee</span>
+            <span>${deliveryFee.toFixed(2)}€</span>
+        </div>
+        <hr class="calc-divider">
+        <div class="calc-total">
+            <span>Total</span>
+            <span>${total.toFixed(2)}€</span>
+        </div>
+        <button class="buy-button">Buy now (${total.toFixed(2)}€)</button>
     `;
 }
