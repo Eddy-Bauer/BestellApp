a534e61876afac117bfc017f35ef0cf7b58c6b2b
Eduard Bauer <eddibauer13@gmail.com>
2026-08-06 22:21:54 +0200

    feat: added 2 categories to menu

--- scripts/db.js
+++ scripts/db.js
@@ -1,24 +1,106 @@
 let menu = {
-  burgers: [
-    {
-      name: "Cheeseburger",
-      price: 9.90,
-      description: "Saftiges Rinderpatty mit Cheddar Cheese und hausgemachter Sauce"
-    },
-    {
-      name: "Hamburger",
-      price: 9.00,
-      description: "Saftiges Rinderpatty mit Tomaten und Salat auf classic Burgersauce"
-    },
-    {
-      name: "Crispy Chicken",
-      price: 9.50,
-      description: "Knusprig paniertes Hähnchenbrustfilet mit Mayo und Eisbergsalat"
-    },
-    {
-      name: "Veggie Burger",
-      price: 8.50,
-      description: "Hausgemachtes Gemüsepatty mit Avocado-Creme und Rucola"
-    }
-  ]
-}
+  burgers: {
+    title: "Burger & Sandwiches",
+    icon: "./assets/icons/burger-icon.png",
+    items: [
+      {
+        id: "burger-1",
+        name: "Veggie mushroom black burger",
+        price: 16.90,
+        description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        image: "./assets/img/veggie-burger.jpg"
+      },
+      {
+        id: "burger-2",
+        name: "All meat burger",
+        price: 15.90,
+        description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+        image: "./assets/img/all-meat-burger.jpg"
+      },
+      {
+        id: "burger-3",
+        name: "Beef red burger",
+        price: 14.90,
+        description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        image: "./assets/img/beef-red-burger.jpg"
+      },
+      {
+        id: "burger-4",
+        name: "Big chicken burger",
+        price: 15.90,
+        description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        image: "./assets/img/big-chicken-burger.jpg"
+      }
+    ]
+  },
+
+  pizza: {
+    title: "Pizza (30cm)",
+    icon: "./assets/icons/pizza-icon.png",
+    items: [
+      {
+        id: "pizza-1",
+        name: "Pizza Margherita",
+        price: 11.90,
+        description: "Tomato Sauce, Mozzarella",
+        image: "./assets/img/pizza-margherita.jpg"
+      },
+      {
+        id: "pizza-2",
+        name: "Pizza Chorizo",
+        price: 13.90,
+        description: "Tomato slices, Mozzarella, Chorizo",
+        image: "./assets/img/pizza-chorizo.jpg"
+      },
+      {
+        id: "pizza-3",
+        name: "Funghi",
+        price: 12.90,
+        description: "Red onion, Olives, Button Mushrooms, Mozzarella",
+        image: "./assets/img/pizza-funghi.jpg"
+      },
+      {
+        id: "pizza-4",
+        name: "Quattro Formaggi with Chicken",
+        price: 15.90,
+        description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        image: "./assets/img/pizza-quattro-formaggi.jpg"
+      }
+    ]
+  },
+
+  salad: {
+    title: "Salad",
+    icon: "./assets/icons/salad-icon.png",
+    items: [
+      {
+        id: "salad-1",
+        name: "Warm beef arugula salad",
+        price: 16.90,
+        description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        image: "./assets/img/warm-beef-arugula-salad.jpg"
+      },
+      {
+        id: "salad-2",
+        name: "Mini green Salad",
+        price: 7.90,
+        description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        image: "./assets/img/mini-green-salad.jpg"
+      },
+      {
+        id: "salad-3",
+        name: "Green Salad with sea food",
+        price: 16.90,
+        description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        image: "./assets/img/green-salad-seafood.jpg"
+      },
+      {
+        id: "salad-4",
+        name: "Vegan green salad with tofu",
+        price: 14.90,
+        description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        image: "./assets/img/vegan-green-salad-tofu.jpg"
+      }
+    ]
+  }
+};
